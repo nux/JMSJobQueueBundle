e2bc0d115af44bdcaecd07e191c5d92fd82f00f8
nux <nux@nux.net>
2016-07-22 09:59:06 -0500

    using bootstrap3view instead of bootstrapview

--- Controller/JobController.php
+++ Controller/JobController.php
@@ -7,7 +7,7 @@ use JMS\DiExtraBundle\Annotation as DI;
 use JMS\JobQueueBundle\Entity\Job;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\Pagerfanta;
-use Pagerfanta\View\TwitterBootstrapView;
+use Pagerfanta\View\TwitterBootstrap3View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -49,7 +49,7 @@ class JobController
         $pager->setCurrentPage(max(1, (integer) $this->request->query->get('page', 1)));
         $pager->setMaxPerPage(max(5, min(50, (integer) $this->request->query->get('per_page', 20))));
 
-        $pagerView = new TwitterBootstrapView();
+        $pagerView = new TwitterBootstrap3View();
         $router = $this->router;
         $routeGenerator = function($page) use ($router, $pager) {
             return $router->generate('jms_jobs_overview', array('page' => $page, 'per_page' => $pager->getMaxPerPage()));
